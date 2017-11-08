f17ee4e54d7cc63ec85f2cca275d7abef44de133
Otari Kobiashvili <otarikobi@gmail.com>
2017-11-08 13:19:45 -0500

    Update gulp to re-build dist/bundle.js after any changes to app/index.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,26 +6,44 @@ import neat from "node-neat";
 import sass from "gulp-sass";
 import webpack from "webpack";
 import WebpackDevServer from "webpack-dev-server";
+import webpackStream from "webpack-stream";
 import webpackConfig from "./webpack.config";
 
 const paths = {
   scss: './public/sass/**/main.scss'
 }
+
+//repurpose sass in to css in public/css folder
 gulp.task('sass', () => {
   return gulp.src(paths.scss)
-      .pipe(sass({
-        sourcemaps: true,
-        includePaths: [bourbon.includePaths, neat.includePaths]
-      }).on('error', sass.logError))
-      .pipe(autoprefix("last 2 versions"))
-      .pipe(gulp.dest('./public/css/'))
+    .pipe(sass({
+      sourcemaps: true,
+      includePaths: [bourbon.includePaths, neat.includePaths]
+    }).on('error', sass.logError))
+    .pipe(autoprefix("last 2 versions"))
+    .pipe(gulp.dest('./public/css/'))
 });
+
+//watch for sass changes, then do sass task
 gulp.task("watch-sass", () => {
   gulp.watch('./public/sass/**/*.scss',['sass']);
 });
 
-gulp.task('webpack-dev-server', function (c) {
-  var myConfig = Object.create(webpackConfig);
+//bundle js to dist folder
+gulp.task("webpack", () => {
+  gulp.src('./app/index.js')
+    .pipe(webpackStream(webpackConfig), webpack)
+    .pipe(gulp.dest('./dist'));
+});
+
+//watch for js changes in app/index.js then do webpack task
+gulp.task("watch-app", ()=> {
+  gulp.watch("./app/index.js", ["webpack"])
+});
+
+//webpack Server
+gulp.task('webpack-dev-server', (c) => {
+  let myConfig = Object.create(webpackConfig);
 
   // Start a webpack-dev-server
   new WebpackDevServer(webpack(myConfig), {
@@ -36,8 +54,8 @@ gulp.task('webpack-dev-server', function (c) {
       if (err) {
         throw new gutil.PluginError('webpack-dev-server', err);
       }
-      gutil.log('[webpack-dev-server]', 'http://localhost:3333/index.html');
+      gutil.log('[webpack-dev-server]', `http://localhost:${myConfig.devServer.port}/index.html`);
   });
 });
 
-gulp.task('default',['webpack-dev-server', "sass", "watch-sass"]);
+gulp.task('default',["watch-app", 'webpack-dev-server', "watch-sass"]);
